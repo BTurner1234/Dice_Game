0ce55199985d4dd6df20c95d7aa848ab420552f9
BTurner1234 <baileyturner@hotmail.co.uk>
2024-05-22 16:23:27 +0100

    Wrote a multiplicative summation function to find the coefficient in the probability function for the formulae

--- dice.py
+++ dice.py
@@ -1,4 +1,20 @@
 import numpy as np
+import math
+
+def mult_sum(num_list):
+  """
+  This function calculates the product of all elements in a list.
+
+  Args:
+      num_list: A list of numbers.
+
+  Returns:
+      The product of all elements in the list.
+  """
+  product = 1
+  for num in num_list:
+    product *= num
+  return product
 
 # Testing example 
 player_1 = [5, 6, 3, 1, 6]
@@ -31,6 +47,12 @@ player_2_num_dice_with_value = np.cumsum(player_2_num_dice_with_value)
 print(player_1)
 print(player_1_num_dice_with_value)
 
+probs_1 = [0]*player_1[0]
+
 for i in range(0, player_1[0]):
-    for j in range(0, i):
-        pass
+    probs_1[i] = 1/mult_sum(player_1[0:player_1_num_dice_with_value[i]])
+    total = 0
+    for j in range(player_1_num_dice_with_value[i]):
+        total +=1
+
+print(probs_1)
